2529916c36d80fbf135753be335402fb193f4085
gabisp28 <gabriela.palma@estudante.ifms.edu.br>
2024-03-13 10:15:05 -0400

    Meu primeiro componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import MeuComponente from './componentes/MeuComponente'
 function App() {
 
     let nome = "Gabriela"
     let sobrenome = "Silva Palma"
     return (
       <div>
+        <MeuComponente/>
      <h1 className="nome">Nome: {nome}</h1>    
      <p className="sobrenome">{sobrenome}</p>    
       </div>
